e07198434f7ac5278f2c3563b6ef5fbe3f34d89a
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-05-20 15:42:33 -0400

    commit tree dce69b1df3cef918a569982f594c9e5db8f0bb15

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "Alex",
+    streetAddress: " 200 New Jersey Road",
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployeeInformation = {... employee}
+    updatedEmployeeInformation[key] = value
+    return updatedEmployeeInformation
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newEmployeeLog = {... employee}
+    newEmployeeLog[key] = value
+    return newEmployeeLog
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    const deleteEmployee =(employee)
+    employee[key]=value
+    return deleteEmployee
+}
